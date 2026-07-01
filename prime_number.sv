c8ee912ac4d796ac6bbf205b00a496808948bacd
CopperHeadd <thobiyas1910@gmail.com>
2026-07-01 17:03:00 +0000

    added prime cheker

---
+++ prime_number.sv
@@ -0,0 +1,19 @@
+module tb_top();
+  int num;
+
+  initial begin
+    check_prime();
+  end
+
+
+
+  function void check_prime();
+    for (num = 1; num <= 100; num++) begin
+      if (is_prime(num)) begin
+        $display("%0d is a prime number.", num);
+      end else begin
+        $display("%0d is not a prime number.", num);
+      end
+    end
+    
+endmodule: tb_top
