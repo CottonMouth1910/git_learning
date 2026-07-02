387db2ce67f1c86fa157839bee2d781a5e53c6b2
CopperHeadd <thobiyas1910@gmail.com>
2026-07-02 15:06:58 +0000

    updated old num array

--- sv/odd_num_array.sv
+++ sv/odd_num_array.sv
@@ -7,6 +7,6 @@ module tb_top();
       arr.sum() with  (item inside {1,2,3,4} ? 1: 0) == arr.size();
     }
 
-    $display("arr : %p", arr);
+    $display("arr : %p", arr); // adding comment from temp branch
   end
 endmodule: tb_top
