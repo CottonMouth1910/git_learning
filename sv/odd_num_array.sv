98783335bbea7f32f2e9f0606257e78789bf62a9
CopperHeadd <thobiyas1910@gmail.com>
2026-07-02 16:26:54 +0000

    adding conflict from questa

--- sv/odd_num_array.sv
+++ sv/odd_num_array.sv
@@ -9,5 +9,6 @@ module tb_top();
 
     $display("arr : %p", arr); // adding comment from questa branch
     $display("inside display");
+    adding conflict comment from questa branch
   end
 endmodule: tb_top
