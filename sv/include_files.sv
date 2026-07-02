5269ca90e3ea02971d0ab62dc5d609f49c6dfbb6
CopperHeadd <thobiyas1910@gmail.com>
2026-07-02 13:54:06 +0000

    added files

---
+++ sv/include_files.sv
@@ -0,0 +1,19 @@
+`ifdef CHECK_ANAGRAM_SV
+  `include "check_anagram.sv"
+`endif
+
+`ifdef CRICKET_SUM_SV
+  `include "cricket_sum.sv"
+`endif
+
+`ifdef ODD_NUM_ARRAY_SV
+  `include "odd_num_array.sv"
+`endif
+
+`ifdef PRIME_NUMBER_SV
+  `include "prime_number.sv"
+`endif
+
+`ifdef SV_CODE1_SV
+  `include "sv_code1.sv"
+`endif
